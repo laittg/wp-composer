e0cd90f6dc577dad62171193bba6b43bc16bbdd9
Laittg <laittg@gmail.com>
2017-12-15 23:59:57 +0700

    Complete Skeleton

---
+++ private/wp-config.sample.php
@@ -0,0 +1,39 @@
+<?php
+// ==================
+// DB connection info
+// ==================
+define( 'DB_NAME', 'wordpress' );
+define( 'DB_USER', 'exampleuser' );
+define( 'DB_PASSWORD', 'examplepassword' );
+define( 'DB_HOST', 'localhost' );
+
+// ===================
+// Hide or show errors
+// ===================
+ini_set( 'display_errors', E_ALL );
+define( 'WP_DEBUG_DISPLAY', true );
+define( 'WP_DEBUG', true );
+
+// =========================
+// Disable automatic updates
+// =========================
+define( 'AUTOMATIC_UPDATER_DISABLED', false );
+
+// ================================
+// Language
+// Leave blank for American English
+// ================================
+define( 'WPLANG', '' );
+
+// ==============================================
+// Salt
+// https://api.wordpress.org/secret-key/1.1/salt/
+// ==============================================
+define('AUTH_KEY',         '');
+define('SECURE_AUTH_KEY',  '');
+define('LOGGED_IN_KEY',    '');
+define('NONCE_KEY',        '');
+define('AUTH_SALT',        '');
+define('SECURE_AUTH_SALT', '');
+define('LOGGED_IN_SALT',   '');
+define('NONCE_SALT',       '');
---
+++ public/wp-config.php
@@ -0,0 +1,46 @@
+<?php
+// =======================================
+// Load wp-config in the private directory
+// =======================================
+define( 'PRIVATE_DIR', dirname( __FILE__ ) . '/../private' );
+
+$wpcfg = array(
+    'dev' => PRIVATE_DIR . '/wp-config.dev.php',
+    'test' => PRIVATE_DIR . '/wp-config.test.php',
+    'production' => PRIVATE_DIR . '/wp-config.php'
+);
+
+if ( file_exists( $wpcfg['dev'] ) ) {
+    define( 'ENVIRONMENT', 'dev' );
+    include( $wpcfg['dev'] );
+} elseif ( file_exists( $wpcfg['test'] ) ) {
+    define( 'ENVIRONMENT', 'test' );
+    include( $wpcfg['test'] );
+} elseif ( file_exists( $wpcfg['production'] ) ) {
+    define( 'ENVIRONMENT', 'production' );
+    include( $wpcfg['production'] );
+} else {
+    die( 'wp-config not found' );
+}
+
+// ========================
+// Custom content directory
+// ========================
+define( 'WP_CONTENT_DIR', dirname( __FILE__ ) . '/wp-content' );
+define( 'WP_CONTENT_URL', 'http://' . $_SERVER['HTTP_HOST'] . '/wp-content' );
+
+// ====================================
+// DB charset, mostly leave it as it is
+// ====================================
+define( 'DB_CHARSET', 'utf8' );
+define( 'DB_COLLATE', '' );
+
+// =======================
+// Load WordPress Settings
+// =======================
+$table_prefix  = 'wp_';
+
+if ( ! defined( 'ABSPATH' ) ) {
+    define( 'ABSPATH', dirname( __FILE__ ) . '/wp/' );
+}
+require_once( ABSPATH . 'wp-settings.php' );
